82cb847ccd82852f9cb0c2bf4e94dad4120875d8
MarcelAzeroth <azerothmarcel@gmail.com>
2022-04-16 21:46:16 +0200

    First Commit

---
+++ main.js
@@ -0,0 +1,59 @@
+//Variables
+let form = document.getElementById("submit-form");
+let submit = document.getElementById("submit");
+let delBtn = document.querySelectorAll(".del-btn");
+let delAll = document.getElementById("delete");
+let itemList = document.getElementById("ul");
+
+
+//Event Listeners
+form.addEventListener("submit", addItem);
+document.body.addEventListener("click", deleteItem)
+ 
+   
+delAll.addEventListener("click", kill);
+
+
+
+//Functions
+
+function addItem(e){
+    //Default
+    e.preventDefault();
+
+    //Variables
+    let newItemValue = document.getElementById("text").value;
+    let li = document.createElement("li");
+    let p = document.createElement("p");
+    let delBtn = document.createElement("button");
+    let itemList = document.getElementById("ul");
+
+    //Modifiy Delete Button
+    delBtn.textContent = "X";
+    delBtn.className ="del-btn";
+
+    //Chain input together
+    p.appendChild(document.createTextNode(newItemValue));
+    li.appendChild(p);
+    li.appendChild(delBtn);
+
+    //Add chained input
+    itemList.appendChild(li);
+
+    //Clear Input
+    document.getElementById("text").value = " ";
+    //Why does this not work
+    // newItemValue = " ";
+}
+
+function deleteItem(e){
+    if(e.target.className === "del-btn"){
+       let li = e.target.parentElement;
+       itemList.removeChild(li);
+    };
+
+}
+
+function kill(e){
+    itemList.textContent = " ";
+}
